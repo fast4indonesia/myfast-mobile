be46a616870fd7d136fc577f6388c12895a2b2e3
Hendy Irawan <ceefour666@gmail.com>
2016-12-15 00:01:52 +0700

    Template with AdminLTE, router, HomeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AlertModule, ButtonsModule } from 'ng2-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    AlertModule
+    RouterModule.forRoot(appRoutes),
+
+    // ng2-bootstrap
+    AlertModule,
+    ButtonsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
